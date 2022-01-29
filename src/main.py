0e7ef471c37e402beefb17285dcf511aced3660b
PhilPinsdorf <philemon.pinsdorf@live.de>
2022-01-29 14:27:55 +0100

    Start Browser with News

---
+++ src/main.py
@@ -0,0 +1,16 @@
+import webbrowser
+import schedule
+import time
+
+
+def open_stream():
+    url = 'https://www.tagesschau.de/multimedia/livestreams/livestream-1-101~player.html'
+    webbrowser.open_new_tab(url)
+
+
+if __name__ == "__main__":
+    schedule.every().day.at("20:00").do(open_stream)
+
+    while 1:
+        schedule.run_pending()
+        time.sleep(1)
